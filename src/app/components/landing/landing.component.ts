8ebaca70601e7c1dfdeef988a13fd6114f12b367
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2023-03-19 17:11:06 +0100

    Désactivation du bouton si pas de texte mais souci avec sélection catégorie qui ne fonctionne pas retour page landing à la soumission du formulaire ok

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ITodo } from 'src/app/interfaces/myTask';
+import { TasksService } from 'src/app/services/tasks.service';
 
 @Component({
   selector: 'app-landing',
@@ -6,9 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing.component.css'],
 })
 export class LandingComponent {
+  // Ma liste de todos
+  todoLists: ITodo[] = [];
+  // J'utilise'mon service
+  constructor(private todo: TasksService) {}
 
   onInit() {
+    //! Je ne récupère rien en console ...
+    this.getTodoList();
   }
-
-
+  // Je récupère la liste des todos
+  getTodoList() {
+    this.todoLists = this.todo.getTodos();
+  }
 }
