261b10898660e41cf38bb6f71b3d16ad6e58845f
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2023-03-23 18:50:02 +0100

    Création méthodes Create, Get et Add pour la liste d'historique/localStorage dans le tasks.service Remontée des datas de la tâche cliquée (click) sur le radioBTN pour alimenter l'historique/localStorage

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ITodo } from 'src/app/interfaces/myTask';
 import { TasksService } from 'src/app/services/tasks.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -12,8 +13,9 @@ export class LandingComponent {
   isHide: boolean = false;
   // Ma liste de todos
   todoLists: ITodo[] = [];
+
   // J'utilise'mon service
-  constructor(private todo: TasksService) {}
+  constructor(private todo: TasksService, private route: Router) {}
 
   ngOnInit() {
     // Je récupère ma liste des tâches
@@ -26,11 +28,23 @@ export class LandingComponent {
   // Méthode pour afficher ou non le bouton + "Aucune tâche pour le moment"
   displayWelcomeMessage() {
     // J'utilise une ternaire plus concise qu'un if => J'ajoute le .length sinon le array vide me fait disparaitre la div
-    this.todoLists.length > 0 ? this.isHide = true : this.isHide = false;
+    this.todoLists.length > 0 ? (this.isHide = true) : (this.isHide = false);
   }
 
   // Je récupère la liste des todos
   getTodoList() {
     this.todoLists = this.todo.getTodos();
+  }
+
+  // Au submit du radioBTN, je capte les données
+  submitButton(hystoric: ITodo) {
+    //* je lis les données captées !!!! Je récupère mon objet !!!
+    console.log(hystoric)
+    // J'envoie mon objet dans mon LS Historique
+    this.todo.addToHystory(hystoric)
+    //! J'efface ma tâche de la liste des tâches
+
+    // Je renvoie à la page de l'historique
+    return this.route.navigate(['myTaskHistory']);
   }
 }
